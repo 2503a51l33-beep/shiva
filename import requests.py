bc062d58f50157346dcec3e706bca7715c19f7eb
2503a51l33-beep <2503a51l33@sru.edu.in>
2025-08-21 15:24:52 +0530

    Add files via upload

---
+++ import requests.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_weather(city, api_key):
+    url = f"https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "metric"
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data
